7dacdb3d31cf292c98bea50e4e75d9a0893fa9b9
Peter Hellberg <peter@c7.se>
2015-01-19 20:43:02 +0100

    Added default gradient

--- gradient/gradient.go
+++ gradient/gradient.go
@@ -1,6 +1,43 @@
 package gradient
 
-import "github.com/lucasb-eyer/go-colorful"
+import (
+	"github.com/lucasb-eyer/go-colorful"
+)
+
+var Default = Table{
+	{Hex("#005994"), 0.00},
+
+	{Hex("#6F8525"), 0.12},
+	{Hex("#B2C85B"), 0.14},
+	{Hex("#EAECB8"), 0.16},
+	{Hex("#002440"), 0.18},
+	{Hex("#005994"), 0.20},
+
+	{Hex("#6F8525"), 0.32},
+	{Hex("#B2C85B"), 0.34},
+	{Hex("#EAECB8"), 0.36},
+	{Hex("#002440"), 0.38},
+	{Hex("#005994"), 0.40},
+
+	{Hex("#F1F334"), 0.52},
+	{Hex("#6B0103"), 0.54},
+	{Hex("#F03C02"), 0.56},
+	{Hex("#1C0113"), 0.58},
+	{Hex("#D30B07"), 0.60},
+
+	{Hex("#002440"), 0.72},
+	{Hex("#005994"), 0.74},
+	{Hex("#004675"), 0.76},
+	{Hex("#002440"), 0.78},
+	{Hex("#005994"), 0.80},
+
+	{Hex("#6F8525"), 0.92},
+	{Hex("#B2C85B"), 0.94},
+	{Hex("#EAECB8"), 0.96},
+	{Hex("#002440"), 0.98},
+
+	{Hex("#005994"), 1.00},
+}
 
 // Table contains the "keypoints" of the colorgradient you want to generate.
 // The position of each keypoint has to live in the range [0,1]
